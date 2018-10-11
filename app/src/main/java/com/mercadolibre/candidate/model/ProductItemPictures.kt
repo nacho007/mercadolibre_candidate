c6fd7524a7ef4047fd9ad5517c61b8735bd8b834
Ignacio Deandreis Denis <ignacio@astropay.com>
2018-10-11 11:43:28 -0300

    Added pictures and detail services

---
+++ app/src/main/java/com/mercadolibre/candidate/model/ProductItemPictures.kt
@@ -0,0 +1,13 @@
+package com.mercadolibre.candidate.model;
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+/**
+ * Created by ignaciodeandreisdenis on 10/11/18.
+ */
+
+class ProductItemPictures(
+        @SerializedName("id") var id: String,
+        @SerializedName("variations") var variations: ArrayList<ProductItemPicture>
+) : Serializable
